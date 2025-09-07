3ef9f917f6c839dbf7c57012451441b839845f53
Adil Khan <Adil03320@gmail.com>
2025-09-07 18:19:08 +0530

    Add SplashScreen widget and update auth state handling in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chatting_app/screens/auth.dart';
 import 'package:chatting_app/screens/chat_screen.dart';
+import 'package:chatting_app/screens/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -26,6 +27,9 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, userSnapshot) {
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          }
           if (userSnapshot.hasData) {
             return const ChatScreen();
           }
