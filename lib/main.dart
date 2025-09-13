0ee5ee5077aef496e9259084effb342633d65319
Adil Khan <Adil03320@gmail.com>
2025-09-13 16:26:24 +0530

    Refactor main initialization: remove Firebase initialization and use AppConfig instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
+import 'package:chatting_app/core/config/app_config.dart';
 import 'package:chatting_app/screens/auth.dart';
 import 'package:chatting_app/screens/chat_screen.dart';
 import 'package:chatting_app/screens/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await AppConfig.initialize();
   runApp(const App());
 }
 
